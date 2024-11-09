40e1c38456d2e2cce25e3167972e3726f649a2cd
aozp73 <aozp73@naver.com>
2024-11-09 16:40:02 +0900

    포트 변경 및 restapi 키 값 환경변수 세팅

--- src/main/java/com/example/demo/controller/KakaoContoller.java
+++ src/main/java/com/example/demo/controller/KakaoContoller.java
@@ -1,6 +1,9 @@
 package com.example.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,46 +14,59 @@ import com.example.demo.service.KakaoService;
 @RestController
 public class KakaoContoller {
 
-	@Autowired
-	public KakaoService kakaoService;
+    private static final Logger LOGGER = LoggerFactory.getLogger(KakaoContoller.class);
 
-	@RequestMapping("/login")
-    public RedirectView goKakaoOAuth() {
-       return kakaoService.goKakaoOAuth();
+    @Value("${rest-RESTAPI_KEY-key}")
+    private String restApiKey;
+
+    @Autowired
+    public KakaoService kakaoService;
+
+    @RequestMapping("/test")
+    public String evnTest() {
+        LOGGER.info("test 진입");
+        LOGGER.info("api 값: {}", restApiKey);
+
+        return "ok";
     }
 
-	@RequestMapping("/login-callback")
-	public RedirectView loginCallback(@RequestParam("code") String code) {
-	   return kakaoService.loginCallback(code);
-	}	
-	
-	@RequestMapping("/profile")
+    @RequestMapping("/login")
+    public RedirectView goKakaoOAuth() {
+        return kakaoService.goKakaoOAuth();
+    }
+
+    @RequestMapping("/login-callback")
+    public RedirectView loginCallback(@RequestParam("code") String code) {
+        return kakaoService.loginCallback(code);
+    }
+
+    @RequestMapping("/profile")
     public String getProfile() {
-       return kakaoService.getProfile();
-    }	
-	
-	@RequestMapping("/authorize")
+        return kakaoService.getProfile();
+    }
+
+    @RequestMapping("/authorize")
     public RedirectView goKakaoOAuth(@RequestParam("scope") String scope) {
-		return kakaoService.goKakaoOAuth(scope);
-    }	
-	
-	@RequestMapping("/friends")
+        return kakaoService.goKakaoOAuth(scope);
+    }
+
+    @RequestMapping("/friends")
     public String getFriends() {
-       return kakaoService.getFriends();
-    }	
-	
-	@RequestMapping("/message")
+        return kakaoService.getFriends();
+    }
+
+    @RequestMapping("/message")
     public String message() {
-       return kakaoService.message();
-    }		
-	
-	@RequestMapping("/friends-message")
+        return kakaoService.message();
+    }
+
+    @RequestMapping("/friends-message")
     public String friends_message(@RequestParam("uuids") String uuids) {
-       return kakaoService.friendMessage(uuids);
-    }	
-	
-	@RequestMapping("/logout")
+        return kakaoService.friendMessage(uuids);
+    }
+
+    @RequestMapping("/logout")
     public String logout() {
-       return kakaoService.logout();
-    }	
+        return kakaoService.logout();
+    }
 }
